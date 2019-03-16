a8b65b7e56b84ceed23d5e0eea4d081102f575c6
Martin Tóth <ezimir@gmail.com>
2019-03-16 15:49:02 +0100

    Display feed list in home view if data present

--- app.py
+++ app.py
@@ -2,17 +2,29 @@
 
 
 
-from flask import Flask
+import json
 
-from flask import render_template
+from flask import Flask
+from flask import flash, render_template
 
 
 
 app = Flask(__name__)
+app.secret_key = 'L4mJRdpkjXLEBG6R#L;y@KiX8'
 
+
+
+DATA_FILE = 'feeds.json'
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    context = {}
+    try:
+        context['feeds'] = json.load(open(DATA_FILE))
+
+    except FileNotFoundError:
+        flash('Data file "{}" not found.'.format(DATA_FILE), 'error')
+
+    return render_template('home.html', **context)
 

7b85322cf42e9dbf8bfdec8d3af081bfe5c61ca5
Martin Tóth <ezimir@gmail.com>
2019-03-16 16:28:16 +0100

    Add CSRF protection to form

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ import json
 
 from flask import Flask, request
 from flask import flash, render_template, redirect, url_for
-from wtforms import Form, validators
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import validators
 from wtforms.fields.html5 import URLField
 
 
 
 app = Flask(__name__)
 app.secret_key = 'L4mJRdpkjXLEBG6R#L;y@KiX8'
+
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 
 
@@ -31,8 +36,8 @@ def home():
     return render_template('home.html', **context)
 
 
-class AddFeedForm(Form):
     url = URLField('URL', [validators.DataRequired()])
+class AddFeedForm(FlaskForm):
 
 @app.route('/add-feed', methods = ['GET', 'POST'])
 def add_feed():

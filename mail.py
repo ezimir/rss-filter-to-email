b1a788616c2697340fb0947cd8ea830592031352
Martin Tóth <ezimir@gmail.com>
2019-03-18 11:13:16 +0100

    Add simple SMTPlib wrapper for sending emails

---
+++ mail.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+
+import os
+import smtplib
+
+import email.message
+import email.headerregistry
+
+
+class Address(email.headerregistry.Address):
+
+    def __init__(self, person):
+        kwargs = {
+            'addr_spec': person,
+        }
+        if isinstance(person, dict):
+            kwargs.update({
+                'display_name': person['name'],
+                'addr_spec': person['address'],
+            })
+
+        super(Address, self).__init__(**kwargs)
+
+
+def send_mail(author, recipient, subject, text, html):
+    msg = email.message.EmailMessage()
+    msg['From'] = Address(author)
+    msg['To'] = [Address(recipient)]
+    msg['Subject'] = subject
+
+    msg.set_content(text)
+    msg.add_alternative(html, subtype = 'html')
+
+    try:
+        server = smtplib.SMTP(
+            os.environ.get('MAIL_SERVER'),
+            os.environ.get('MAIL_PORT'),
+        )
+        server.send_message(msg)
+
+    finally:
+        server.quit()
+
